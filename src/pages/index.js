3f63466a6d8649e79d86f76ec985cbdea2b5c25c
arima0k <tecstim@gmail.com>
2022-06-08 12:46:30 -0500

    Modify index

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container" style={Roboto}>
-        <h1 className="hero__title">Luis Vera |<span style={{fontSize: "60%", color: "#57001b" }}> AKA arima0k / angw97</span></h1>
+        <h1 className="hero__title">Luis Vera
+            |<span style={{fontSize: "60%", color: "#57001b" }}> AKA arima0k / angw97</span></h1>
         <h2 className="hero__subtitle">Personal Website</h2>
         <p className="hero__subtitle">Enthusiast of technology that helps make the world a better place.</p>
       </div>
@@ -26,11 +27,8 @@ function Technology() {
     <div className="hero hero--dark" style={Roboto}>
       <div className="container">
         <h1 className="hero__title">Why tecstim?</h1>
-        <p className={"hero__subtitle"}>Origin Etymology:</p>
-        <p>Technology means "science of craft", from Greek τέχνη, <span style={{color: '#e17a9b'}}><b>tec</b></span>hne,
-        "art, skill, cunning of hand"; and -λογία, -logia.</p>
-         <p>And from Ancient Greek κατάστημα (katá<span style={{color: '#e17a9b'}}><b>stim</b></span>a)</p>
-
+        <p className="hero__subtitle" style={{fontSize: "99%"}}>Technology means "science of craft", from Greek τέχνη, <span style={{color: '#e17a9b'}}><b>tec</b></span>hne,
+        "art, skill, cunning of hand"; and -λογία, -logia. • And κατάστημα (katá<span style={{color: '#e17a9b'}}><b>stim</b></span>a), shop, store. </p>
       </div>
     </div>
     );
